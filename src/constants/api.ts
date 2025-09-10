a79b70a51bcb107bfb8e3eae37535c94534f468c
whobuythis <yunchanwo@naver.com>
2025-09-10 10:47:41 +0900

    Feat & Docs: 백엔드 환경변수 설정 및 Readme.md 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 // API 관련 상수
 export const API_CONFIG = {
-  BASE_URL: "http://10.10.0.86:4000",
+  BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://10.10.0.85:4000",
   // "http://ec2-15-164-151-28.ap-northeast-2.compute.amazonaws.com",
   TIMEOUT: 10000,
   HEADERS: {
